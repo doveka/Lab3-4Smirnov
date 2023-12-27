43a38c8cbfed8ebb78e627a6be399b17856a0dc7
doveka <131176206+doveka@users.noreply.github.com>
2023-12-28 01:08:31 +0200

    Add files via upload

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,8 @@
+﻿double y;
+Console.WriteLine("|     x     |     y     |");
+for (double i = 1; i <= 3; i+=0.2)
+{
+    y = Math.Pow(i, 3) - 7 * i - 7;
+    Console.WriteLine();
+    Console.WriteLine($"|  {i:F2}  |  {y:F2}  |");
+}
